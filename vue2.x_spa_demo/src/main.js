6e21002149a3dbb4016e34691682a93edfcc7d53
zhangyi <1090362846@qq.com>
2017-01-06 20:17:29 +0800

    submit

--- vue2.x_spa_demo/src/main.js
+++ vue2.x_spa_demo/src/main.js
@@ -4,20 +4,21 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 import App from './app.vue';
-import zy from './components/zy.vue';
-import js from './components/js.vue';
+import ZY from './components/zy.vue';
+import JS from './components/js.vue';
 
 
 const routes = [
     //设置根目录时候渲染谁，即默认情况下渲染哪个组件
-    {path:'/',component:zy},
+    // {path:'/',component:zy},
     //路由重定向
-    // {path:'/',redirect:'/zy'},
-    {path:'/zy',component:zy}, 
-    {path:'/js',component:js}, 
+    {path:'/',redirect:'/ZY'},
+    {path:'/ZY',component:ZY}, 
+    {path:'/JS',component:JS},
 ];
 
 const router = new VueRouter({
+    // mode: 'history',
     routes
 });
 
