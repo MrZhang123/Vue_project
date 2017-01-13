2b486b35ba9af26797aad46786cb50cbb35f1e67
zhangyi <1090362846@qq.com>
2017-01-13 12:25:19 +0800

    submit

--- vue2.x_spa_demo/src/components/demo/propsDemo.vue
+++ vue2.x_spa_demo/src/components/demo/propsDemo.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="props-demo">
-        <h4>props</h4>
-        <div class="weui-cells__title">文本框</div>
+        <div class="page__hd">
+            <h1 class="page__title">props</h1>
+            <p class="page__desc">props的一些用法</p>
+        </div>
+        <div class="weui-cells__title">动态props</div>
         <div class="weui-cells weui-cells_form">
             <div class="weui-cell">
                 <div class="weui-cell__bd">
@@ -12,10 +15,27 @@
                     v-model="parentMsg">
                 </div>
             </div>
-            <div class="weui-cell weui-cell_access">
+            <div class="weui-cell">
+                <div class="weui-cell__bd">普通输出：<child :input-message="parentMsg"></child></div>
+            </div>
+            <div class="weui-cell">
+                <div class="weui-cell__bd">计算后输出：<child-secound :input-message="parentMsg" ref="childSecound"></child-secound></div>
+            </div>
+        </div>
+        <div class="weui-cells__title">单向数据流</div>
+        <div class="weui-cells weui-cells_form">
+            <div class="weui-cell">
+                <div class="weui-cell__bd">
+                    <input 
+                    class="weui-input" 
+                    type="text" 
+                    placeholder="请输入"
+                    v-model="parentMsg">
+                </div>
+            </div>
+            <div class="weui-cell">
                 <div class="weui-cell__bd">输出：<child :input-message="parentMsg"></child></div>
             </div>
-            
         </div>
     </div>
 </template>
@@ -30,7 +50,23 @@ export default{
         child:{
             props:['inputMessage'],
             template:'<span>{{inputMessage}}</span>'
+        },
+        'child-secound':{
+            props:['inputMessage'],
+            template:'<span>{{lowerCase}}</span>',
+            computed:{
+                lowerCase(){
+                    return this.$refs;
+                    // return this.inputMessage.toLowerCase();
+                }
+            }    
         }
-    }
+        
+    },
 }
 </script>
+<style scoped>
+.props-demo h2{
+    padding: 20px;
+}
+</style>
