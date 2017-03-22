6f9e28b4dcadd205b498d12cf7314bf5d0bb9ba0
zhangyi <1090362846@qq.com>
2017-03-22 19:20:53 +0800

    distinguish between webpack.dev.config & webpack.build.config.js

---
+++ vue2.x/webpack.build.config.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+    entry: {
+        app: ['./src/main.js'],
+        vendor: [
+            'lodash',
+            'axios',
+            'vue',
+            'vue-router'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        publicPath: '/dist',
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            //转化ES6语法
+            {
+                test: /\.js$/,
+                use: 'babel',
+                /*以上为简写
+                use: [
+                  {
+                    loader: 'babel'
+                  }
+                  //or
+                  'babel'
+                ],*/
+                exclude: /node_modules/
+            },
+            //解析.vue文件
+            {
+                test: /\.vue$/,
+                loader: 'vue',
+                options: {
+                    css: extractTextPlugin.extract({
+                        fallback: 'vue-style-loader',
+                        use: 'css-loader'
+                    })
+                }
+            },
+            //图片转化，小于8K自动转化为base64的编码
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url',
+                options: {
+                    limit: 8192
+                }
+            }
+        ]
+    },
+    //禁止显示webpack的build.js太大的提示
+    performance: {
+        hints: false
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        new extractTextPlugin({
+            filename: 'style.css',
+            allChunks: true
+        }),
+        new htmlWebpackPlugin({
+            filename: 'assets/index.html'
+        })
+    ],
+    resolveLoader: {
+        moduleExtensions: ['-loader']
+    },
+    resolve: {
+        // require时省略的扩展名，如：require('app') 不需要app.js
+        extensions: ['.js', '.vue'],
+        // 别名，可以直接使用别名来代表设定的路径以及其他
+        alias: {
+            'vue$': 'vue/dist/vue.common.js',
+            components: path.join(__dirname, './src/components')
+        }
+    }
+}
---
+++ vue2.x/webpack.dev.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const extractTextPlugin = require('extract-text-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+module.exports = {
+    entry: {
+        app: ['./src/main.js'],
+        vendor: [
+            'lodash',
+            'axios',
+            'vue',
+            'vue-router'
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname + '/dist'),
+        publicPath: '/dist',
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            //转化ES6语法
+            {
+                test: /\.js$/,
+                use: 'babel',
+                exclude: /node_modules/
+            },
+            /*以上为简写
+            use: [
+            {
+                loader: 'babel'
+            }
+            //or
+            'babel'
+            ],*/
+            //解析.vue文件
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            },
+            //图片转化，小于8K自动转化为base64的编码
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: 'url',
+                options: {
+                    limit: 8192
+                }
+            }
+        ]
+    },
+    //禁止显示webpack的build.js太大的提示
+    performance: {
+        hints: false
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor'
+        })
+    ],
+    resolveLoader: {
+        moduleExtensions: ['-loader']
+    },
+    resolve: {
+        // require时省略的扩展名，如：require('app') 不需要app.js
+        extensions: ['.js', '.vue'],
+        // 别名，可以直接使用别名来代表设定的路径以及其他
+        alias: {
+            'vue$': 'vue/dist/vue.common.js',
+            components: path.join(__dirname, './src/components')
+        }
+    }
+}
