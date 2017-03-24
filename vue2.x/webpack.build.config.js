3c611068bb552f96e70a4864e50229fe4f2223f7
zhangyi <1090362846@qq.com>
2017-03-24 15:04:35 +0800

    sub

--- vue2.x/webpack.build.config.js
+++ vue2.x/webpack.build.config.js
@@ -77,7 +77,8 @@ module.exports = {
         }),
         new htmlWebpackPlugin({
             title:'SPA',
-            filename: 'assets/index.html'
+            filename: 'assets/index.html',
+            inject:'body'
         })
     ],
     resolveLoader: {
